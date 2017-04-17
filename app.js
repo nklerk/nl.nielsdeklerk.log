efeab665fbc4e99cb303ea538db10e970611a298
Niels de Klerk <Niels.dKlerk@vened.nl>
2017-04-17 13:38:06 +0200

    Changes for app store

--- app.js
+++ app.js
@@ -5,24 +5,18 @@ module.exports = {
 	init: function () { }
 }
 
-/*Homey.manager('flow').on('action.Input_log', function( callback, args ){
-    Homey.log('Log: ' + args);
-    callback( null, true ); // we've fired successfully
-});*/
-
 // when a flow is saved with a new trigger
 Homey.manager('flow').on('action.Input_log', function( callback, args ) {
     Homey.log('Log : ' + args.log);
     // always fire the callback, it's reserved for future argument validation
-    var value = "";
-    var mylog = Homey.manager('settings').get( 'myLog' );
-    if (mylog != undefined) { 
-        value = args.log + "\n" + mylog;
+    var logNew = "";
+    var logOld = Homey.manager('settings').get( 'myLog' );
+    if (logOld != undefined) { 
+        logNew = args.log + "\n" + logOld;
     } else {
-        value = args.log;
+        logNew = args.log;
     }
    
-    Homey.manager('settings').set( 'myLog', value /* Must be 'JSON.stringify'-able */ );
-    Homey.log(value); // myValue
+    Homey.manager('settings').set( 'myLog', logNew );
     callback( null, true );
 });
